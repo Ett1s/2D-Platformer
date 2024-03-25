53c4e9320f2be6f6424bf262af861fce73c8d5d6
gashiwingo <gashiwingo@gmail.com>
2024-03-25 15:22:15 +0300

    'Enemy'

--- Assets/LOCATION_CITY/CharacterController.cs
+++ Assets/LOCATION_CITY/CharacterController.cs
@@ -7,29 +7,57 @@ public class CharacterController : MonoBehaviour
 {
     private Animator animator;
     private SpriteRenderer sprite;
+    private Rigidbody2D rb;
     private float moveSpeed = 2f;
+    private float runSpeed = 10f;
+    private float jumpForce = 5f;
+    private bool isGrounded = false;
 
     void Start()
     {
         animator = GetComponent<Animator>();
         sprite = GetComponentInChildren<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
+        animator.SetBool("attack", false);
         float horizontalInput = Input.GetAxis("Horizontal");
 
         if (horizontalInput != 0)
         {
             Vector3 dir = transform.right * Input.GetAxis("Horizontal");
-            transform.position = Vector3.MoveTowards(transform.position, transform.position + dir, moveSpeed * Time.deltaTime);
+            float speed = Input.GetKey(KeyCode.LeftShift) ? runSpeed : moveSpeed;
+            transform.position = Vector3.MoveTowards(transform.position, transform.position + dir, speed * Time.deltaTime);
             sprite.flipX = dir.x < 0.0f;
 
-            animator.SetBool("IsWalking", true);
+            animator.SetBool("running", true);
         }
         else
         {
-            animator.SetBool("IsWalking", false);
+            animator.SetBool("running", false);
+        }
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            isGrounded = false;
+            animator.SetTrigger("Jump");
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            animator.SetBool("attack", true);
+        }
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
         }
     }
 }

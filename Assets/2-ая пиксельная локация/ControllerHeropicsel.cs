6f0462884677596d3df28082a73736a341ab3ed9
Kirill <kirillgeroy@bk.ru>
2024-03-28 23:18:48 +0300

    .

---
+++ Assets/2-ая пиксельная локация/ControllerHeropicsel.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D;
+
+public class ControllerHeropicsel : MonoBehaviour
+{
+    private Animator animator;
+    private SpriteRenderer sprite;
+    private Rigidbody2D rb;
+    [SerializeField]private float moveSpeed = 5f;
+    [SerializeField]private float runSpeed = 10f;
+    [SerializeField]private float jumpForce = 7f;
+    private bool isGrounded = false;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        sprite = GetComponentInChildren<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        animator.SetBool("attack", false);
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        if (horizontalInput != 0)
+        {
+            Vector3 dir = transform.right * Input.GetAxis("Horizontal");
+            float speed = Input.GetKey(KeyCode.LeftShift) ? runSpeed : moveSpeed;
+            transform.position = Vector3.MoveTowards(transform.position, transform.position + dir, speed * Time.deltaTime);
+            sprite.flipX = dir.x < 0.0f;
+
+            animator.SetBool("running", true);
+        }
+        else
+        {
+            animator.SetBool("running", false);
+        }
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            isGrounded = false;
+            animator.SetTrigger("Jump");
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            animator.SetBool("attack", true);
+        }
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+}
--- Assets/LOCATION_CITY/CharacterController.cs
+++ Assets/LOCATION_CITY/CharacterController.cs
@@ -7,57 +7,29 @@ public class CharacterController : MonoBehaviour
 {
     private Animator animator;
     private SpriteRenderer sprite;
-    private Rigidbody2D rb;
     private float moveSpeed = 2f;
-    private float runSpeed = 10f;
-    private float jumpForce = 5f;
-    private bool isGrounded = false;
 
     void Start()
     {
         animator = GetComponent<Animator>();
         sprite = GetComponentInChildren<SpriteRenderer>();
-        rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        animator.SetBool("attack", false);
         float horizontalInput = Input.GetAxis("Horizontal");
 
         if (horizontalInput != 0)
         {
             Vector3 dir = transform.right * Input.GetAxis("Horizontal");
-            float speed = Input.GetKey(KeyCode.LeftShift) ? runSpeed : moveSpeed;
-            transform.position = Vector3.MoveTowards(transform.position, transform.position + dir, speed * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, transform.position + dir, moveSpeed * Time.deltaTime);
             sprite.flipX = dir.x < 0.0f;
 
-            animator.SetBool("running", true);
+            animator.SetBool("IsWalking", true);
         }
         else
         {
-            animator.SetBool("running", false);
-        }
-
-        if (Input.GetButtonDown("Jump") && isGrounded)
-        {
-            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            isGrounded = false;
-            animator.SetTrigger("Jump");
-        }
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            animator.SetBool("attack", true);
-        }
-
-    }
-
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            isGrounded = true;
+            animator.SetBool("IsWalking", false);
         }
     }
 }
